7f55469370abe01022598fdf1e45588be2b3a6a4
KoT <68163043+ArchdukeOfTrondelag@users.noreply.github.com>
2025-07-20 09:36:08 +0200

    Add files via upload

--- configs/term/.rgfzf.sh
+++ configs/term/.rgfzf.sh
@@ -1,2 +1,2 @@
 ord=$(fzf-tmux -p 35,4 --reverse --prompt="Searchword: " --print-query | awk 'NR==1' )
-rg -l "$ord" | fzf-tmux -p 80%,80% --delimiter : --preview "bat --color=always  --highlight-line=1 {1} "| xargs nvim
+rg -l -. --iglob '!.oh-my-zsh/*' --iglob '!.tmux/*' --iglob '!.local/*' --iglob '!/Vieb/*' "$ord" | fzf-tmux -p 80%,80% --delimiter : --preview "bat --color=always  --highlight-line=1 {1} "| xargs nvim
