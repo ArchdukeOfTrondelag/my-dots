846964c604931c9eab3932bd393fe2ea8a318ff1
KoT <68163043+ArchdukeOfTrondelag@users.noreply.github.com>
2025-07-23 01:00:20 +0200

    Add files via upload

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,29 +1,35 @@
 require('plugins')
 
 vim.cmd([[
+
 nnoremap <C-k> :FzfLuaFiles<CR>
 nnoremap <M-k> :FzfLua files ~/.cache/nvim/tnew/<CR>
-nnoremap <C-s> :w<CR>
+nnoremap <C-l> :FzfLua buffers<CR>
+
 nnoremap <C-n> :bn<CR>
 nnoremap <C-c> :bdelete<CR>
 nnoremap <M-c> :bdelete!<CR>
-nnoremap <C-a> :Startify<CR>
-nnoremap <M-m> :set cursorline<CR>
-nnoremap <M-n> :set nocursorline<CR>
+
 nnoremap <M-s> :w<CR>
 nnoremap <M-w> :q<CR>
-nnoremap <F5> :Tnew<CR>
-nnoremap <F6> :terminal<CR>
+
+nnoremap <F2> :Tnew<CR>
+
+nnoremap <F3> :terminal<CR>
 tnoremap <M-Esc> <C-\><C-n>
-nnoremap <C-l> :FzfLua buffers<CR>
 
 autocmd VimEnter * hi Visual guifg=magenta  
+
+set cursorline
 set scrolloff=6
 set t_Co=16
+
 set number
 set relativenumber
+
 set undodir=~/.vim/undo-di      "set where the undo file is stored
 set undofile                    "sets a file where undos are saved, so if i undo and quit i can redo later
+
 ]])
 
 vim.g.nord_italic = false
