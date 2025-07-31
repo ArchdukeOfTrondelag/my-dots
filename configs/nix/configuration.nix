3d98c7e5f0f82eaca92644be889b24706c248954
KoT <68163043+ArchdukeOfTrondelag@users.noreply.github.com>
2025-07-31 21:22:22 +0200

    Add files via upload

--- configs/nix/configuration.nix
+++ configs/nix/configuration.nix
@@ -4,11 +4,13 @@
 		[ # Include the results of the hardware scan.
 		./hardware-configuration.nix
 		];
+	programs.nh = {
+		enable = true;
+		clean.enable = true;
+		clean.extraArgs = "--keep-since 4d --keep 3";
+	};
 
-	boot.loader.grub.enable = true;
-	boot.loader.grub.device = "/dev/sda";
-	boot.loader.grub.useOSProber = true;
-
+	boot.loader.systemd-boot.enable = true;
 	networking.hostName = "nixos"; # Define your hostname.
 		networking.networkmanager.enable = true;
 
@@ -23,7 +25,8 @@
 	console.keyMap = "no";
 	services.printing.enable = true;
 
-	nix.settings.experimental-features = [ "nix-command" "flakes"];
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
 
 	programs.zsh.enable = true;
 	users.defaultUserShell = pkgs.zsh;
@@ -37,6 +40,34 @@
 		pulse.enable = true;
 	};
 
+
+	hardware.graphics = {
+		enable = true;
+	};
+	hardware.nvidia = {
+		modesetting.enable = true;
+		powerManagement.enable = false;
+		powerManagement.finegrained = false;
+		open = true;
+		nvidiaSettings = true;
+		package = config.boot.kernelPackages.nvidiaPackages.stable;
+	};
+
+	hardware.nvidia.prime = {
+		intelBusId = "PCI:00:02:0";
+		nvidiaBusId = "PCI:01:0:0";
+	};
+
+
+	users.users.pix = {
+		isNormalUser = true;
+		description = "pix";
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; [
+		steam
+		prismlauncher
+		];
+	};
 	users.users.nix = {
 		isNormalUser = true;
 		description = "nix";
@@ -48,39 +79,77 @@
 	nixpkgs.config.allowUnfree = true;
 	environment.systemPackages = with pkgs; [
 
-			fd
+		fd
 			zip
 			unzip
+			hyfetch
+			fastfetch
+			btop
 			bat
+
+			feh
+			swww
+			picom
+			kitty
 			cron
 			at
 
-			w3m
-			mutt
-
 			khal
 			cmatrix
-			fastfetch
-			btop
+			gimp
 
 			gcc
+			vlang
+			tree-sitter
+			wget
+			gnumake
 			lua
 
 			fzf
 			neovim
 			git
 			tmux
+			ripgrep
 
-			wiki-tui
-
-			cage
 			vieb
-			cmus
-			foot
+			brave
+			xwayland-satellite
+			polybar
+			waybar
+			rofi
+			fuzzel
+			arandr
+			swaylock
+
+			wf-recorder
+			simplescreenrecorder
 			mpv
+
+			openttd-jgrpp
 			];
-		programs.niri.enable = true;
+
+	services.xserver = {
+		enable = true;
+		displayManager.gdm.enable= true;
+		desktopManager = {
+			xterm.enable = false;
+			xfce= {
+				enable = true;
+				noDesktop = true;
+			};
+		};
+		windowManager.i3.enable = true;
+	};
+
+	programs.niri.enable = true;
+
+	programs.virt-manager.enable = true;
+	users.groups.libvirtd.members = ["nix"];
+	virtualisation.libvirtd.enable = true;
+	virtualisation.spiceUSBRedirection.enable = true;
 
 	system.stateVersion = "25.05"; # Did you read the comment?
 
 }
+
+
