e3d349e34bb643b1621595bea0191f16dfedb425
KoT <68163043+ArchdukeOfTrondelag@users.noreply.github.com>
2025-08-13 19:45:43 +0200

    Add files via upload

--- configs/home/.rgfzf.sh
+++ configs/home/.rgfzf.sh
@@ -84,85 +84,84 @@ ls_1() {
   ls "$dir"
 }
 
-ls_2() {
-  dir=$(fd --type d --hidden | fzf-tmux -p)
-  l "$dir"
+bat_1() {
+  fil=$( fd --type f --hidden | fzf-tmux -p -w 35% -h 25% -p --reverse --no-preview )
+  bat "$fil"
 }
 
+
 cmd_1() {
-  echo " find -finds file based on prmpt word "
-  echo " cd -fuzzy find and change directory "
-  echo " open -finds and opens a file in nvim "
-  echo " co -fuzzy find directory and make file in that directory with nvim "
-  echo " md -make directory in input directory and cd into it "
-  echo " df -cd into directory and make file, but doesnt open "
-  echo " ls -fuzzy find directory and ls, lists the content "
-  echo " l -fuzzy find directory and l, lists the content with hidden files "
-  echo " mf -makes file in given directory and you remain in your directory "
-  echo " mm -moves picked directory 1 to picked directory 2 "
-  echo " fm -moves file into picked directory "
-  echo " fc -copes file into picked directory "
-  echo " fd -copes directory into picked directory "
+  echo " "
+  echo " find  - finds file based on prmpt word "
+  echo " cd    - fuzzy find and change directory "
+  echo " open  - finds and opens a file in nvim "
+  echo " co    - fuzzy find directory and make file in that directory with nvim "
+  echo " md    - make directory in input directory and cd into it "
+  echo " df    - cd into directory and make file, but doesnt open "
+  echo " ls    - fuzzy find directory and ls, lists the content "
+  echo " mf    - makes file in given directory and you remain in your directory "
+  echo " mm    - moves picked directory 1 to picked directory 2 "
+  echo " fm    - moves file into picked directory "
+  echo " fc    - copes file into picked directory "
+  echo " fd    - copes directory into picked directory "
+  echo " bat   - opens file in bat "
 }
 
 
 main () {
 
-case "$1" in
-  fc)
-    cp_3
-    ;;
-  fm)
-    cp_2
-    ;;
-  mm)
-    cp_1
-    ;;
-  find)
-    fnd_1
-    ;;
-  cd)
-    cd_1
-    ;;
-  co)
-    co_1
-    ;;
-  open)
-    open_1
-    ;;
-  md)
-    cd_2
-    ;;
-  mf)
-    mf_1
-    ;;
-  l)
-    ls_2
-    ;;
-  ls)
-    ls_1
-    ;;
-  --help)
-    help_1
-    ;;
-  fd)
-    fd_1
-    ;;
-  df)
-    co_2
-    ;;
-  cmd)
-    cmd_1
-    ;;
-  *)
-    echo "Unknown command: $1"
-    ;;
-esac
+  case "$1" in
+    bat)
+      bat_1
+      ;;
+    fc)
+      cp_3
+      ;;
+    fm)
+      cp_2
+      ;;
+    mm)
+      cp_1
+      ;;
+    find)
+      fnd_1
+      ;;
+    cd)
+      cd_1
+      ;;
+    co)
+      co_1
+      ;;
+    open)
+      open_1
+      ;;
+    md)
+      cd_2
+      ;;
+    mf)
+      mf_1
+      ;;
+    ls)
+      ls_1
+      ;;
+    --help)
+      help_1
+      ;;
+    fd)
+      fd_1
+      ;;
+    df)
+      co_2
+      ;;
+    cmd)
+      cmd_1
+      ;;
+  esac
 
 }
 
 
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main "$@"
-fi
+# if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+#     main "$@"
+# fi
