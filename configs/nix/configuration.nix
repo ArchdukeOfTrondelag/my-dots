c0ce5e4c6a2090560c960b51e904ab3205d15dec
KoT <68163043+ArchdukeOfTrondelag@users.noreply.github.com>
2025-09-07 14:53:32 +0200

    Add files via upload

--- configs/nix/configuration.nix
+++ configs/nix/configuration.nix
@@ -1,14 +1,22 @@
 {config, pkgs, ... }:
+# let
+# home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
+# in
+let
+nixowos = import (builtins.fetchGit {
+    url = "https://github.com/yunfachi/nixowos";
+    });
+in
 {
   imports =
     [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
+      nixowos.nixosModules.default
+# (import "${home-manager}/nixos")
     ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
+  boot.loader.systemd-boot.enable = true;
+  services.displayManager.enable = true;
   networking.hostName = "nixos"; # Define your hostname.
     networking.networkmanager.enable = true;
 
@@ -20,14 +28,12 @@
     variant = "nodeadkeys";
   };
 
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-tty;
-  };
-
   console.keyMap = "no";
   services.printing.enable = true;
-  nix.settings.experimental-features = [ "nix-command" "flakes"];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  programs.steam.enable = true;
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
@@ -40,73 +46,121 @@
     pulse.enable = true;
   };
 
+  nixowos.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      open = true;
+      modesetting.enable = true;
+      nvidiaSettings = true;
+      powerManagement.enable = false;
+      package = config.boot.kernelPackages.nvidiaPackages.stable; # Same as production
+    };
+  };
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:00:02:0";
+    nvidiaBusId = "PCI:01:0:0";
+  };
+
   users.users.nix = {
     isNormalUser = true;
     description = "nix";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
   };
 
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
 
-    xwayland-satellite
+    openttd-jgrpp
+      wl-clipboard
+      cowsay
+      lolcat
 
-      fd
-      zip
-      unzip
-      fbcat
-      bat
-      ripgrep
-      pandoc
-      gnumake
-
-      cmatrix
-      hyfetch
-      fastfetch
-      btop
+      vieb
+      firefox
+      gimp
+      foot
+      nautilus
 
       gcc
+      tree-sitter
+      wget
+      gnumake
+
+      python3
       lua
+      cargo
       rust-analyzer
       rustup
-      cargo
       lua-language-server
       bash-language-server
       nil
 
-      fzf
-      neovim
-      vim
-      git
-      tmux
-      mpv
+      xwayland-satellite
+      polybar
+      waybar
+      rofi
+      fuzzel
+      picom
+      arandr
+      swaylock
+      cmatrix
+      feh
+      swww
 
-      cage
-      mailutils
-      openssl
-      msmtp
       wf-recorder
       ffmpeg
-      vieb
-      qutebrowser
-      foot
-      fuzzel
-      curl
-      wget
-      wl-clipboard
-      swww
-      ];
+      simplescreenrecorder
+      mpv
+      pavucontrol
+      pulsemixer
 
-  programs.niri.enable = true;
+      vim
+      neovim
+      fzf
+      git
+      tmux
+      nix-output-monitor
+      ripgrep
+      fd
+      cron
+      at
+      zip
+      unzip
+      hyfetch
+      fastfetch
+      btop
+      bat
+      ];
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce= {
+        enable = true;
+        noDesktop = true;
+      };
+    };
+    windowManager.i3.enable = true;
+  };
   services.displayManager.ly.enable = true;
+
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
              url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
              }))
   ];
 
-  system.stateVersion = "25.05"; # Did you read the comment?
+  programs.niri.enable = true;
 
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["nix"];
+  virtualisation.libvirtd.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  system.stateVersion = "25.05"; # Did you read the comment?
 }
+
+
